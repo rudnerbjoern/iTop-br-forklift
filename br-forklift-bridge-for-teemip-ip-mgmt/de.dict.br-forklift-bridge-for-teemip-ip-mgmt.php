82ca3cd44bef30f68152a393bf5d98ae493d13e3
Bjoern Rudner <rudnerbjoern@hotmail.com>
2024-09-04 13:52:27 +0200

    Add bridge for TeemIp IP Management

---
+++ br-forklift-bridge-for-teemip-ip-mgmt/de.dict.br-forklift-bridge-for-teemip-ip-mgmt.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @copyright   Copyright (C) 2024 Björn Rudner
+ * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
+ * @version     2024-09-04
+ *
+ * Localized data
+ */
+
+Dict::Add('DE DE', 'German', 'Deutsch', array(
+    'Class:Forklift/Attribute:macaddress' => 'MAC Adresse',
+    'Class:Forklift/Attribute:macaddress+' => '',
+    'Class:Forklift/Attribute:ipaddress_id' => 'IP Adresse',
+    'Class:Forklift/Attribute:ipaddress_id+' => '',
+    'Class:Forklift/Attribute:ipaddress_name' => 'IP Adressname',
+    'Class:Forklift/Attribute:ipaddress_name+' => '',
+));
---
+++ br-forklift-bridge-for-teemip-ip-mgmt/module.br-forklift-bridge-for-teemip-ip-mgmt.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @copyright   Copyright (C) 2024 Björn Rudner
+ * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
+ * @version     2024-09-04
+ *
+ * iTop module definition file
+ */
+
+SetupWebPage::AddModule(
+    __FILE__,
+    'br-forklift-bridge-for-teemip-ip-mgmt/0.0.1',
+    array(
+        // Identification
+        //
+        'label' => 'Bridge - BR-Forklift + TeemIp IP Management',
+        'category' => 'business',
+
+        // Setup
+        //
+        'dependencies' => array(
+            'itop-endusers-devices/2.7.0',
+            'br-forklift/0.0.1||teemip-ip-mgmt/3.0.0',
+            'br-forklift/0.0.1||teemip-config-mgmt-adaptor/3.0.0',
+            'br-forklift/0.0.1',
+        ),
+        'mandatory' => false,
+        'visible' => true, // To prevent auto-install but shall not be listed in the install wizard
+        'auto_select' => 'SetupInfo::ModuleIsSelected("itop-endusers-devices") && SetupInfo::ModuleIsSelected("teemip-ip-mgmt") && SetupInfo::ModuleIsSelected("teemip-config-mgmt-adaptor") && SetupInfo::ModuleIsSelected("br-forklift")',
+
+        // Components
+        //
+        'datamodel' => array(
+            'model.br-forklift-bridge-for-teemip-ip-mgmt.php',
+        ),
+        'data.struct' => array(),
+        'data.sample' => array(),
+
+        // Documentation
+        //
+        'doc.manual_setup' => '',
+        'doc.more_information' => '',
+
+        // Default settings
+        //
+        'settings' => array(),
+    )
+);
