70858ccad0b0fcd6aa743a26d329fd879c09121e
Bjoern Rudner <rudnerbjoern@hotmail.com>
2024-09-05 17:45:48 +0200

    enhance compatibility to iTop 2.7

--- br-forklift-bridge-for-teemip-ip-mgmt/module.br-forklift-bridge-for-teemip-ip-mgmt.php
+++ br-forklift-bridge-for-teemip-ip-mgmt/module.br-forklift-bridge-for-teemip-ip-mgmt.php
@@ -3,14 +3,14 @@
 /**
  * @copyright   Copyright (C) 2024 Björn Rudner
  * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
- * @version     2024-09-04
+ * @version     2024-09-05
  *
  * iTop module definition file
  */
 
 SetupWebPage::AddModule(
     __FILE__,
-    'br-forklift-bridge-for-teemip-ip-mgmt/0.1.0',
+    'br-forklift-bridge-for-teemip-ip-mgmt/2.7.1',
     array(
         // Identification
         //
@@ -21,9 +21,9 @@ SetupWebPage::AddModule(
         //
         'dependencies' => array(
             'itop-endusers-devices/2.7.0',
-            'br-forklift/0.1.0||teemip-ip-mgmt/3.0.0',
-            'br-forklift/0.1.0||teemip-config-mgmt-adaptor/3.0.0',
-            'br-forklift/0.1.0',
+            'br-forklift/2.7.1||teemip-ip-mgmt/3.0.0',
+            'br-forklift/2.7.1||teemip-config-mgmt-adaptor/3.0.0',
+            'br-forklift/2.7.1',
         ),
         'mandatory' => false,
         'visible' => true, // To prevent auto-install but shall not be listed in the install wizard
